d4450cf6a1d54f78579135ad863d3bd2688520b7
Kumar_Anshuman <kanshuman6484@gmail.com>
2025-04-18 19:35:43 +0530

    added login, signup and directory

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,45 @@
-import "./App.css";
+import React, { useState } from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import LoginPage from './pages/LoginPage'
+import SignUpPage from './pages/SignUpPage'
+import Navbar from './components/Navbar'
+import DirectoryPage from './pages/DirectoryPage'
 
-function App() {
+const App = () => {
+  const [darkMode, setDarkMode] = useState(false)
+
+  const toggleDarkMode = () => {
+    setDarkMode((prev) => {
+      const newMode = !prev
+      if (newMode) {
+        document.documentElement.classList.add('dark')
+      } else {
+        document.documentElement.classList.remove('dark')
+      }
+      return newMode
+    })
+  }
+
   return (
-    <>
-      <h1 className='text-bold bg-green-500'>Hello</h1>
-    </>
-  );
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route
+          path="/login"
+          element={
+            <LoginPage toggleDarkMode={toggleDarkMode} darkMode={darkMode} />
+          }
+        />
+        <Route
+          path="/signup"
+          element={
+            <SignUpPage toggleDarkMode={toggleDarkMode} darkMode={darkMode} />
+          }
+        />
+        <Route path="/directory" element={<DirectoryPage />} />
+      </Routes>
+    </Router>
+  )
 }
 
-export default App;
+export default App
